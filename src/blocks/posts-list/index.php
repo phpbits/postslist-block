f2a378d20b19a6b57e62adf9de48f457c2330687
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-10-29 22:09:36 +0800

    Add initial frontend display

--- src/blocks/posts-list/index.php
+++ src/blocks/posts-list/index.php
@@ -11,8 +11,82 @@
  * @return string Returns the block content.
  */
 function wpengine_render_postslist_block( $attributes ) {
+	global $post;
+
+	/* Setup the query */
+	$postsListQuery = new WP_Query(
+		array(
+			'posts_per_page'      => $attributes['postsToShow'],
+			'post_status'         => 'publish',
+			'ignore_sticky_posts' => 1,
+			'post__not_in'        => array( $post->ID ), // Exclude the current post from the grid.
+		)
+	);
+
+	$html = '<section class="'. ( isset( $attributes['className'] ) ? $attributes['className'] : '' ) .'">';
+
+	/* Start the loop */
+	if ( $postsListQuery->have_posts() ) {
+		while ( $postsListQuery->have_posts() ) {
+			$postsListQuery->the_post();
+
+			/* Setup the post ID */
+			$post_id = get_the_ID();
+
+			/* Setup the featured image ID */
+			$post_thumb_id = get_post_thumbnail_id( $post_id );
+
+			/* Get the post title */
+			$title = get_the_title( $post_id );
+
+			if ( ! $title ) {
+				$title = __( 'Untitled', 'postslist-block' );
+			}
+
+			$post_classes = '';
+
+			/* Add sticky class */
+			if ( is_sticky( $post_id ) ) {
+				$post_classes .= ' sticky';
+			} else {
+				$post_classes .= null;
+			}
+			/* Join classes together */
+			$post_classes = join( ' ', get_post_class( $post_classes, $post_id ) );
+
+			$html .= sprintf(
+				'<article id="post-%1$s" class="%2$s">',
+				esc_attr( $post_id ),
+				esc_attr( $post_classes )
+			);
+
+			$html .= '<header class="entry-header">';
+				$html .= sprintf(
+					'<h2><a href="%1$s" rel="bookmark">%2$s</a></h2>',
+					esc_url( get_permalink( $post_id ) ),
+					esc_html( $title )
+				);
+			$html .= '</header>';
+
+			if( $post_thumb_id ){
+				/* Display the featured image */
+				$html .= sprintf(
+					'<div class="entry-media"><a href="%1$s" class="post-thumbnail" rel="bookmark" aria-hidden="true" tabindex="-1">%2$s</a></div>',
+					esc_url( get_permalink( $post_id ) ),
+					wp_get_attachment_image( $post_thumb_id, 'full' )
+				);
+			}
+			
+
+			$html .= '</article>';
+		}
+		/* Restore original post data */
+		wp_reset_postdata();
+	}
+
+	$html .= '</section>';
 	
-	return 'test';
+	return $html;
 }
 /**
  * Registers the block on server.
