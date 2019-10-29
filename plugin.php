85c33ddd6f65e6f87bf11c77babc3ab8773fffb4
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-10-29 14:10:05 +0800

    Add packages and file structures

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Posts List Block
+ * Plugin URI: http://github.com/phpbits/postslist-block
+ * Description: Gutenberg Posts List Block displays carousel posts list with user-defined number of posts .
+ * Version: 1.0
+ * Author: Jeffrey Carandang
+ * Author URI: https://jeffreycarandang.com/
+ * Text Domain: postslist-block
+ * Domain Path: languages
+ *
+ * @category Gutenberg
+ * @author Jeffrey Carandang
+ * @version 1.0
+ */
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! class_exists( 'EditorsKit' ) ) :
+/**
+ * Main EditorsKit Class.
+ *
+ * @since  1.0
+ */
+
+endif;
+?>
